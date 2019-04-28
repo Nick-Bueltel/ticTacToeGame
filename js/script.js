af71db1b7abbf3324d64722feec07ea8e655525d
Nick-Bueltel <nickbueltel@gmail.com>
2019-04-28 10:15:18 -0500

    started game logic, created layout

--- js/script.js
+++ js/script.js
@@ -1,2 +1,30 @@
-alert("js loaded successfully ");
-alert("test ")
+//constants 
+const PIECES = {
+    '1': "X",
+    '-1': "O",
+    '0': null; 
+}
+
+//state var 
+var board, winner, turn; 
+
+
+//cache 
+
+//event listeners 
+
+//functions 
+init();
+
+function handleClick(evt){
+
+
+    
+    turn *= -1; 
+    render();
+
+
+}
+
+
+// 
